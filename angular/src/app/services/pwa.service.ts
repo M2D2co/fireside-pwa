259a34687201189c1a12d93a362cb7c57feb8c5c
Martine Dowden <martine@andromeda16.com>
2024-08-22 02:26:26 -0500

    fix pwa (#2)

--- angular/src/app/services/pwa.service.ts
+++ angular/src/app/services/pwa.service.ts
@@ -1,9 +1,10 @@
 import { Platform } from '@angular/cdk/platform';
-import { Injectable } from '@angular/core';
+import { ApplicationRef, Injectable } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { timer } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { filter, first, take } from 'rxjs/operators';
 import { InstallPromptComponent } from '../core/install-prompt/install-prompt.component';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
 
 @Injectable({
   providedIn: 'root'
@@ -13,26 +14,33 @@ export class PwaService {
   constructor(
     private platform: Platform,
     private bottomSheet: MatBottomSheet,
-  ) { }
+    private appRef: ApplicationRef,
+    swUpdate: SwUpdate,
+  ) {
+    //  checks for new version
+    swUpdate.versionUpdates
+      .pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'))
+      .subscribe(() => {
+        alert('A new version is available, app will reload to update to the latest version.')
+        document.location.reload()
+      })
+  }
 
   public initPwaPrompt(): void {
     window.addEventListener('beforeinstallprompt', event => {
       //  TODO
-      console.log(this.platform)
       if (this.platform.ANDROID || this.platform.BLINK) {
         event.preventDefault()
         this.openPromptComponent('android', event);
       } else if (this.platform.IOS) {
         const isInStandaloneMode = ('standalone' in window.navigator) && ((window.navigator as any)['standalone']);
-        if (!isInStandaloneMode) {
-          this.openPromptComponent('ios', event);
-        }
+        if (!isInStandaloneMode) { this.openPromptComponent('ios', event); }
       }
     })
   }
 
   private openPromptComponent(mobileType: 'ios' | 'android', event: Event): void {
-    timer(3000).pipe(take(1)).subscribe(() => {
+    this.appRef.isStable.pipe(first(isStable => isStable === true)).subscribe(() => {
       this.bottomSheet.open(InstallPromptComponent, { data: { mobileType, promptEvent: event } });
     })
   }
